728a940887298d7fcbca872dad62a933273216c9
OskHe <75313192+OskHe@users.noreply.github.com>
2022-06-16 16:13:09 -0400

    Fixed Radar
    jcenter -> mavenCentral()

--- src/main/java/oskhe/meteorextension/modules/hud/RadarHud.java
+++ src/main/java/oskhe/meteorextension/modules/hud/RadarHud.java
@@ -20,7 +20,10 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.util.math.Vec3d;
+import oskhe.meteorextension.MeteorExtension;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.Iterator;
 
 public class RadarHud extends HudElement {
@@ -74,9 +77,9 @@ public class RadarHud extends HudElement {
             .build()
     );
 
-    public final Setting<Boolean> ignoreSelf = sgGeneral.add(new BoolSetting.Builder()
-        .name("ignore-self")
-        .description("Ignores yourself drawing the shader.")
+    public final Setting<Boolean> drawSelf = sgGeneral.add(new BoolSetting.Builder()
+        .name("draw-self")
+        .description("Wether to draw you on the Radar")
         .defaultValue(true)
         //.visible(() -> mode.get() == ESP.Mode.Shader)
         .build()
@@ -109,6 +112,8 @@ public class RadarHud extends HudElement {
         .build()
     );
 
+    private static long lastMillis = 0;
+
     public RadarHud(HUD hud) {
         super(hud, "Radar", "Shows a Radar on your HUD thar tells you where entities are.");
     }
@@ -132,28 +137,84 @@ public class RadarHud extends HudElement {
             Renderer2D.COLOR.render(null);
 
             //Renderer2D.COLOR.
-            renderer.text("0", 0 + x + box.width/2, 0 + y + box.height/2, Color.WHITE);
-            renderer.text("5", 5 + x + box.width/2, 5 + y + box.height/2, Color.WHITE);
+            //renderer.text("0", 0 + x + box.width/2, 0 + y + box.height/2, Color.WHITE);
+            //renderer.text("5", 5 + x + box.width/2, 5 + y + box.height/2, Color.WHITE);
+
+            if (drawSelf.get()) {
+                renderer.text(character.get().toString(), box.width/2 + x, box.height/2 + y, esp.getColor(mc.player));
+            }
+
+            renderer.text("X", box.width/2 + x + 200, box.height/2 + y, esp.getColor(mc.player));
+            renderer.text("Y", box.width/2 + x, box.height/2 + y + 200, esp.getColor(mc.player));
+
+
+            //boolean printed = false;
+            int num = 0;
 
             if (mc.world != null) {
                 for (Entity entity : mc.world.getEntities()) {
                     if (shouldSkip(entity)) continue;
 
+                    assert mc.player != null;
+
                     double xPos = ((entity.getX() - mc.player.getX()) * scale.get() * zoom.get()/* + box.width/2*/);
                     double yPos = ((entity.getZ() - mc.player.getZ()) * scale.get() * zoom.get()/*  + box.height/2*/);
+                    //BigDecimal BDxPos = new BigDecimal(xPos);
+                    //BigDecimal BDyPos = new BigDecimal(yPos);
 
-                    double radius = Math.sqrt(xPos*xPos + yPos*yPos);
+                    /*if (xPos > 0) {
+                        yPos *= -1d;
+                        //xPos *= -1d;
+                    }*/
+
+                    double dist = Math.sqrt(xPos*xPos + yPos*yPos);
+                    //BigDecimal dist = new BigDecimal(0);
+                    //dist = BDxPos.pow(2).add(BDyPos.pow(2)).sqrt(MathContext.DECIMAL128);
+
                     double theta = Math.atan(yPos/xPos);
+                    //BigDecimal theta = new BigDecimal(0);
+                    //theta = new BigDecimal(Math.atan(BDyPos.divide(BDxPos).doubleValue()));
 
-                    double yaw = mc.player.getHeadYaw() + 180; //0-360
-                    theta = theta - (yaw * Math.PI/180);
+                    if (xPos > 0) {
+                        theta += Math.PI;
+                    }
 
-                    xPos = radius * Math.cos(theta);
-                    yPos = radius * Math.sin(theta);
+                    double yaw = mc.player.getHeadYaw() % 360; //0-360
+                    //BigDecimal BDyaw = new BigDecimal(mc.player.getHeadYaw());
+                    theta = theta - (yaw * (Math.PI/180d));
+                    //theta = theta.subtract(BDyaw.multiply(BigDecimal.valueOf(Math.PI/180d)));
+
+                    /*if (xPos > 0) {
+                        theta *= -1d;
+                    }*/
+
+                    if (num++ == 2 && System.currentTimeMillis()-lastMillis >= 200) {
+
+                        //renderer.text("distance: " + dist, 0 + x + box.width/2, 0 + y + box.height/2, Color.WHITE);
+                        //renderer.text("theta: " + theta, 0 + x + box.width/2, 20 + y + box.height/2, Color.WHITE);
+                        MeteorExtension.LOG.info("xPos: {}, yPos: {}, distance: {}, theta: {}, thetaold: {}", s(xPos), s(yPos), s(dist), s(theta), s(Math.atan(yPos/xPos)));
+
+                        lastMillis = System.currentTimeMillis();
+                    }
+                    //num ++;
+
+                    //theta -= Math.PI/2;
+
+                    double xPosOld = xPos;
+                    double yPosOld = yPos;
+
+
+
+
+                    xPos = dist * Math.cos(theta);
+                    yPos = dist * Math.sin(theta);
+
+                    /*if (xPosOld > 0) {
+                        yPos *= -1d;
+                    }*/
 
                     xPos += box.width/2;
                     yPos += box.height/2;
-
 
                     if (xPos < 0 || yPos < 0 || xPos > box.width - scale.get() || yPos > box.height - scale.get()) continue;
 
@@ -193,8 +254,13 @@ public class RadarHud extends HudElement {
 
     }
 
+    private String s(double d) {
+        return "%.2f".formatted(d);
+    }
+
     private boolean shouldSkip(Entity entity) {
         if (!entities.get().getBoolean(entity.getType())) return true;
+
         return !EntityUtils.isInRenderDistance(entity);
     }
 
