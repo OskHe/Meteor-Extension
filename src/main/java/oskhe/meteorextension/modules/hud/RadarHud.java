fd2778633667f6706f7008c03e5e3169ecb47068
OskHe <75313192+OskHe@users.noreply.github.com>
2022-06-16 17:54:59 -0400

    Code Cleanup

--- src/main/java/oskhe/meteorextension/modules/hud/RadarHud.java
+++ src/main/java/oskhe/meteorextension/modules/hud/RadarHud.java
@@ -27,11 +27,12 @@ import java.math.MathContext;
 import java.util.Iterator;
 
 public class RadarHud extends HudElement {
-    public enum AvailableCharacters {//*•◦+
+    public enum AvailableCharacters {//*●•◦+
         STAR("*"),
         POINT("•"),
         CIRCLE("◦"),
-        PLUS("+");
+        PLUS("+"),
+        DOT("●");
 
         private String character = "";
 
@@ -49,50 +50,12 @@ public class RadarHud extends HudElement {
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    private final Setting<Object2BooleanMap<EntityType<?>>> entities = sgGeneral.add(new EntityTypeListSetting.Builder()
-        .name("entities")
-        .description("Select entities to be drawn.")
-        .defaultValue(EntityType.PLAYER)
-        .build()
-    );
-
-    private final Setting<SettingColor> backgroundColor = sgGeneral.add(new ColorSetting.Builder()
-        .name("background-color")
-        .description("The color of the background.")
-        .defaultValue(new SettingColor(0, 0, 0, 64))
-        .build()
-    );
-
-    /*private final Setting<Boolean> letters = sgGeneral.add(new BoolSetting.Builder()
-            .name("letters")
-            .description("Use entity's type first letter.")
-            .defaultValue(true)
-            .build()
-    );*/
-
-    private final Setting<Boolean> showWaypoints = sgGeneral.add(new BoolSetting.Builder()
-            .name("waypoints")
-            .description("Show waypoints.")
-            .defaultValue(false)
-            .build()
-    );
-
-    public final Setting<Boolean> drawSelf = sgGeneral.add(new BoolSetting.Builder()
-        .name("draw-self")
-        .description("Wether to draw you on the Radar")
-        .defaultValue(true)
-        //.visible(() -> mode.get() == ESP.Mode.Shader)
-        .build()
-    );
-
-
-
     private final Setting<Double> scale = sgGeneral.add(new DoubleSetting.Builder()
         .name("scale")
         .description("The scale.")
         .defaultValue(1)
         .min(1)
-        .sliderRange(0.01, 5)
+        .sliderRange(0.01, 10)
         .build()
     );
 
@@ -112,7 +75,34 @@ public class RadarHud extends HudElement {
         .build()
     );
 
-    private static long lastMillis = 0;
+    private final Setting<Object2BooleanMap<EntityType<?>>> entities = sgGeneral.add(new EntityTypeListSetting.Builder()
+        .name("entities")
+        .description("Select entities to be drawn.")
+        .defaultValue(EntityType.PLAYER)
+        .build()
+    );
+
+    private final Setting<SettingColor> backgroundColor = sgGeneral.add(new ColorSetting.Builder()
+        .name("background-color")
+        .description("The color of the background.")
+        .defaultValue(new SettingColor(0, 0, 0, 64))
+        .build()
+    );
+
+    public final Setting<Boolean> drawSelf = sgGeneral.add(new BoolSetting.Builder()
+        .name("draw-self")
+        .description("Wether to draw you on the Radar")
+        .defaultValue(true)
+        .build()
+    );
+
+    private final Setting<Boolean> showWaypoints = sgGeneral.add(new BoolSetting.Builder()
+        .name("waypoints")
+        .description("Show waypoints.")
+        .defaultValue(false)
+        .build()
+    );
+
 
     public RadarHud(HUD hud) {
         super(hud, "Radar", "Shows a Radar on your HUD thar tells you where entities are.");
@@ -120,7 +110,7 @@ public class RadarHud extends HudElement {
 
     @Override
     public void update(HudRenderer renderer) {
-        box.setSize(100 * scale.get(), 100 * scale.get());
+        box.setSize(200 * scale.get(), 200 * scale.get());
     }
 
     @Override
@@ -136,20 +126,9 @@ public class RadarHud extends HudElement {
             Renderer2D.COLOR.quad(x, y, box.width, box.height, backgroundColor.get());
             Renderer2D.COLOR.render(null);
 
-            //Renderer2D.COLOR.
-            //renderer.text("0", 0 + x + box.width/2, 0 + y + box.height/2, Color.WHITE);
-            //renderer.text("5", 5 + x + box.width/2, 5 + y + box.height/2, Color.WHITE);
-
             if (drawSelf.get()) {
                 renderer.text(character.get().toString(), box.width/2 + x, box.height/2 + y, esp.getColor(mc.player));
             }
-
-            renderer.text("X", box.width/2 + x + 200, box.height/2 + y, esp.getColor(mc.player));
-            renderer.text("Y", box.width/2 + x, box.height/2 + y + 200, esp.getColor(mc.player));
-
-
-            //boolean printed = false;
-            int num = 0;
 
             if (mc.world != null) {
                 for (Entity entity : mc.world.getEntities()) {
@@ -157,74 +136,29 @@ public class RadarHud extends HudElement {
 
                     assert mc.player != null;
 
-                    double xPos = ((entity.getX() - mc.player.getX()) * scale.get() * zoom.get()/* + box.width/2*/);
-                    double yPos = ((entity.getZ() - mc.player.getZ()) * scale.get() * zoom.get()/*  + box.height/2*/);
-                    //BigDecimal BDxPos = new BigDecimal(xPos);
-                    //BigDecimal BDyPos = new BigDecimal(yPos);
-
-                    /*if (xPos > 0) {
-                        yPos *= -1d;
-                        //xPos *= -1d;
-                    }*/
+                    double xPos = ((entity.getX() - mc.player.getX()) * scale.get() * zoom.get());
+                    double yPos = ((entity.getZ() - mc.player.getZ()) * scale.get() * zoom.get());
 
                     double dist = Math.sqrt(xPos*xPos + yPos*yPos);
-                    //BigDecimal dist = new BigDecimal(0);
-                    //dist = BDxPos.pow(2).add(BDyPos.pow(2)).sqrt(MathContext.DECIMAL128);
 
                     double theta = Math.atan(yPos/xPos);
-                    //BigDecimal theta = new BigDecimal(0);
-                    //theta = new BigDecimal(Math.atan(BDyPos.divide(BDxPos).doubleValue()));
 
                     if (xPos > 0) {
                         theta += Math.PI;
                     }
 
                     double yaw = mc.player.getHeadYaw() % 360; //0-360
-                    //BigDecimal BDyaw = new BigDecimal(mc.player.getHeadYaw());
                     theta = theta - (yaw * (Math.PI/180d));
-                    //theta = theta.subtract(BDyaw.multiply(BigDecimal.valueOf(Math.PI/180d)));
-
-                    /*if (xPos > 0) {
-                        theta *= -1d;
-                    }*/
-
-                    if (num++ == 2 && System.currentTimeMillis()-lastMillis >= 200) {
-
-                        //renderer.text("distance: " + dist, 0 + x + box.width/2, 0 + y + box.height/2, Color.WHITE);
-                        //renderer.text("theta: " + theta, 0 + x + box.width/2, 20 + y + box.height/2, Color.WHITE);
-                        MeteorExtension.LOG.info("xPos: {}, yPos: {}, distance: {}, theta: {}, thetaold: {}", s(xPos), s(yPos), s(dist), s(theta), s(Math.atan(yPos/xPos)));
-
-                        lastMillis = System.currentTimeMillis();
-                    }
-                    //num ++;
-
-                    //theta -= Math.PI/2;
-
-                    double xPosOld = xPos;
-                    double yPosOld = yPos;
-
-
-
 
                     xPos = dist * Math.cos(theta);
                     yPos = dist * Math.sin(theta);
-
-                    /*if (xPosOld > 0) {
-                        yPos *= -1d;
-                    }*/
 
                     xPos += box.width/2;
                     yPos += box.height/2;
 
                     if (xPos < 0 || yPos < 0 || xPos > box.width - scale.get() || yPos > box.height - scale.get()) continue;
 
-                    String icon = character.get().toString();
-
-                    /*if (letters.get())
-                        icon = entity.getType().getUntranslatedName().substring(0,1).toUpperCase();
-                    */
-                    renderer.text(icon, xPos + x, yPos + y, esp.getColor(entity));
-                    //renderer.
+                    renderer.text(character.get().toString(), xPos + x, yPos + y, esp.getColor(entity));
                 }
             }
 
@@ -236,16 +170,29 @@ public class RadarHud extends HudElement {
                     Vec3 c = waypoint.getCoords();
                     Vec3d coords = new Vec3d(c.x, c.y, c.z);
 
-                    double xPos = ((coords.getX() - mc.player.getX()) * scale.get() * zoom.get() + box.width/2);
-                    double yPos = ((coords.getZ() - mc.player.getZ()) * scale.get() * zoom.get()  + box.height/2);
+                    double xPos = ((coords.getX() - mc.player.getX()) * scale.get() * zoom.get());
+                    double yPos = ((coords.getZ() - mc.player.getZ()) * scale.get() * zoom.get());
+
+                    double dist = Math.sqrt(xPos*xPos + yPos*yPos);
+
+                    double theta = Math.atan(yPos/xPos);
+
+                    if (xPos > 0) {
+                        theta += Math.PI;
+                    }
+
+                    double yaw = mc.player.getHeadYaw() % 360; //0-360
+                    theta = theta - (yaw * (Math.PI/180d));
+
+                    xPos = dist * Math.cos(theta);
+                    yPos = dist * Math.sin(theta);
+
+                    xPos += box.width/2;
+                    yPos += box.height/2;
 
                     if (xPos < 0 || yPos < 0 || xPos > box.width - scale.get() || yPos > box.height - scale.get()) continue;
 
-                    String icon = "*";
-                    /*if (letters.get() && waypoint.name.length() > 0)
-                        icon = waypoint.name.substring(0, 1);*/
-
-                    renderer.text(icon, xPos + x, yPos + y, waypoint.color);
+                    renderer.text(AvailableCharacters.DOT.toString(), xPos + x, yPos + y, waypoint.color);
                 }
             }
 
@@ -254,14 +201,11 @@ public class RadarHud extends HudElement {
 
     }
 
-    private String s(double d) {
-        return "%.2f".formatted(d);
-    }
 
     private boolean shouldSkip(Entity entity) {
-        if (!entities.get().getBoolean(entity.getType())) return true;
-
-        return !EntityUtils.isInRenderDistance(entity);
+        //if (!entities.get().getBoolean(entity.getType())) return true;
+        //return !EntityUtils.isInRenderDistance(entity);
+        return !entities.get().getBoolean(entity.getType());
     }
 
 }
